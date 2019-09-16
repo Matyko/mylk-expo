e6a7b73bf0e07c62c0b4bc7de6352d8329ad3e0f
Matyas Krista <matyas@geomiq.com>
2019-09-16 17:46:59 +0100

    Settings screen bases started

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {View, StyleSheet, Text, Switch} from "react-native";
+import Colors from "../constants/Colors";
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+      <View style={styles.container}>
+        <View style={styles.settingsContent}>
+          <View style={styles.settingsElement}>
+            <Text style={styles.settingsTitle}>
+              Something to set
+            </Text>
+            <View style={styles.functionality}>
+              <Switch/>
+            </View>
+          </View>
+        </View>
+        <View style={styles.settingsFooter}>
+          <Text style={styles.footerText}>Mylk by Matyi</Text>
+        </View>
+      </View>
+  )
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: 'Settings',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  settingsContent: {
+    flexGrow: 1
+  },
+  settingsFooter: {
+    flexGrow: 0,
+    height: 79,
+    backgroundColor: Colors.primaryBackground
+  },
+  settingsElement: {
+    flexDirection: 'row'
+  },
+  functionality: {
+    flexGrow: 0,
+    width: 100
+  },
+  settingsTitle: {
+    flexGrow: 1
+  },
+  footerText: {
+    lineHeight: 79,
+    color: Colors.white
+  }
+});
